5809ecd3bcb122a1fe118440a854b654d3629d07
Denis Chumakov <denis.chumakov@exness.com>
2022-09-10 21:44:54 +0700

    'deploy'

---
+++ precache-manifest.7111ffa0cd73688590fa412eded86c1d.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2f7c3d17bf27a237a5223b42f7bd00",
+    "url": "/404.html"
+  },
+  {
+    "revision": "85479cf6359f1f47f04cac341892c2ec",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "56cb4fdd61cb4d6e6a1b38cb7af8a823",
+    "url": "/bsc.json"
+  },
+  {
+    "revision": "c71d8214bac707c63210",
+    "url": "/css/app.cc1f8927.css"
+  },
+  {
+    "revision": "17a107c9f1b6371bbe85",
+    "url": "/css/chunk-vendors.3b17dfd8.css"
+  },
+  {
+    "revision": "aa32e8b5b9e0f242828b2282a1e61e1c",
+    "url": "/img/coins/Binance_logo.svg"
+  },
+  {
+    "revision": "44677679216079ff49abe8bff51a1dfe",
+    "url": "/img/coins/Binance_logo_white.svg"
+  },
+  {
+    "revision": "4e7df8616bc6a9f4a191ef50a425ee97",
+    "url": "/img/coins/RusCoin.svg"
+  },
+  {
+    "revision": "c5b12e2f59618f5951419b6bfc29e8a6",
+    "url": "/img/coins/RusCoinBack.svg"
+  },
+  {
+    "revision": "d7e3ad6a4ec2674aa09067992fc2987a",
+    "url": "/img/coins/RusCoinBlue.svg"
+  },
+  {
+    "revision": "ddd9575254797f47b9f9d2e3f5a39718",
+    "url": "/img/coins/RusCoinDark.svg"
+  },
+  {
+    "revision": "6d9e7a5ee6becce6823a77352860aafb",
+    "url": "/img/coins/RusCoinOnWhite.svg"
+  },
+  {
+    "revision": "8146bc9d89ac676e0f45d0d79e0d890d",
+    "url": "/img/coins/RusCoinWhite.svg"
+  },
+  {
+    "revision": "3740239d74858504f5345365a1e3eb33",
+    "url": "/img/coins/diagram/BNB.png"
+  },
+  {
+    "revision": "14baed95bed1d4a24dc256844db15244",
+    "url": "/img/coins/diagram/BUSD.png"
+  },
+  {
+    "revision": "f0af8e3c5f8d91c80989550790dd0d6f",
+    "url": "/img/coins/diagram/CAKE.png"
+  },
+  {
+    "revision": "e28cc4b74e8a839059bf6e57b22de986",
+    "url": "/img/coins/diagram/HERO.png"
+  },
+  {
+    "revision": "69ceb2284787446cb3e9dacb918e6e6b",
+    "url": "/img/coins/diagram/NEAR.png"
+  },
+  {
+    "revision": "2e2ca3a2d6d7505467606481c8f90574",
+    "url": "/img/coins/diagram/PVM.png"
+  },
+  {
+    "revision": "d2a40d9a4dfd82486a88213ff2155e73",
+    "url": "/img/coins/diagram/RUSCOIN.png"
+  },
+  {
+    "revision": "739797800c1ad83ba9a3cdddb9824693",
+    "url": "/img/coins/diagram/TRX.png"
+  },
+  {
+    "revision": "8e0ae3a81575d146c935a79ba2d0222b",
+    "url": "/img/coins/diagram/diagram.png"
+  },
+  {
+    "revision": "79dcd4e07593611c7dbdaaab02100077",
+    "url": "/img/coins/logo-exchange.svg"
+  },
+  {
+    "revision": "503b45695112550de4af6971e6db4481",
+    "url": "/img/coins/relictum.svg"
+  },
+  {
+    "revision": "fbcb1822ee283973d21377edc112e22c",
+    "url": "/img/coins/relictum_logo.svg"
+  },
+  {
+    "revision": "5f446146e2483742eb890d3717bbfe7b",
+    "url": "/img/coins/uniswap.svg"
+  },
+  {
+    "revision": "b7c54a63043d37734e7c3d37fed7edbb",
+    "url": "/img/coins/waves.svg"
+  },
+  {
+    "revision": "fc9a00174d46b8fbdbc7e5470c18622c",
+    "url": "/img/coins/zhcash.png"
+  },
+  {
+    "revision": "c879a582a94e772b8ed27295ae83145d",
+    "url": "/img/common/Fox.svg"
+  },
+  {
+    "revision": "2fcd18f829dadea59815853f03cb046b",
+    "url": "/img/common/metamaskInstr.png"
+  },
+  {
+    "revision": "3fedcd7478c014928b42e605186c5396",
+    "url": "/img/common/relictum_acc.jpg"
+  },
+  {
+    "revision": "76d9e5b5378d38ca3ec2580c5643b6e4",
+    "url": "/img/common/relictum_addr.jpg"
+  },
+  {
+    "revision": "0751c6ffc7eecd485d1e90687219c472",
+    "url": "/img/flags/ar.svg"
+  },
+  {
+    "revision": "622ba8870651eb4065a366cd8517b7eb",
+    "url": "/img/flags/au.svg"
+  },
+  {
+    "revision": "719d4486d02e0b65dce63b53645f9cb7",
+    "url": "/img/flags/by.svg"
+  },
+  {
+    "revision": "ca3817520f5d0f2c47f25c0af894cfb2",
+    "url": "/img/flags/ca.svg"
+  },
+  {
+    "revision": "cf2ee9cebdaf163fcd2d573d1fa0bc38",
+    "url": "/img/flags/ch.svg"
+  },
+  {
+    "revision": "c7cee5e9b2c5dc743f254a54c5320039",
+    "url": "/img/flags/cn.svg"
+  },
+  {
+    "revision": "a9fc0bda59e60be25196f7f613f34d15",
+    "url": "/img/flags/de.svg"
+  },
+  {
+    "revision": "c89ee484a8023861e5c33c6f29636266",
+    "url": "/img/flags/dk.svg"
+  },
+  {
+    "revision": "478d0d0999b6ac573981e5243f9a486b",
+    "url": "/img/flags/en.svg"
+  },
+  {
+    "revision": "7afd79c899f275f2a4d58ab3bc142ea9",
+    "url": "/img/flags/es.svg"
+  },
+  {
+    "revision": "be825203afebca1b3d8cf26cdc67d60f",
+    "url": "/img/flags/fr.svg"
+  },
+  {
+    "revision": "0f8a7acc1ce7c49554ba1014013a1e8e",
+    "url": "/img/flags/ge.svg"
+  },
+  {
+    "revision": "74b1a9fbd993211a13eb54637504e82e",
+    "url": "/img/flags/hk.svg"
+  },
+  {
+    "revision": "3dfd2434cf2518b5cb5822175836cbd8",
+    "url": "/img/flags/ke.svg"
+  },
+  {
+    "revision": "90326d72103f548254919b41669dfd39",
+    "url": "/img/flags/kr.svg"
+  },
+  {
+    "revision": "0b74d901e95542988a8bf2d2b30808b6",
+    "url": "/img/flags/no.svg"
+  },
+  {
+    "revision": "03ade50f6e2c48f734cbe7496252ab66",
+    "url": "/img/flags/pl.svg"
+  },
+  {
+    "revision": "02989ed6b1fd68a6c6838ecf125566fd",
+    "url": "/img/flags/ru.svg"
+  },
+  {
+    "revision": "4a944ffe6d3cd77f02bd8fce4271b068",
+    "url": "/img/flags/se.svg"
+  },
+  {
+    "revision": "6e20dab74361cd13f39d03a24dfc42aa",
+    "url": "/img/flags/si.svg"
+  },
+  {
+    "revision": "a5c6a8fca29c06685f45aaa14f720f58",
+    "url": "/img/flags/sk.svg"
+  },
+  {
+    "revision": "70436af370f372b2b436ab81df6139f1",
+    "url": "/img/flags/sn.svg"
+  },
+  {
+    "revision": "5cae0909806cb9e262c447cd2a793c5f",
+    "url": "/img/flags/tn.svg"
+  },
+  {
+    "revision": "dd0fd49842a897a28b1f30522ef8f1c2",
+    "url": "/img/flags/tr.svg"
+  },
+  {
+    "revision": "80a665877d8653e646123e84b4483265",
+    "url": "/img/flags/ve.svg"
+  },
+  {
+    "revision": "26d8c0153769e5d0a96d410625309606",
+    "url": "/img/flags/za.svg"
+  },
+  {
+    "revision": "ead8e6f416f8350de5f4d6c89c0152c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c71d8214bac707c63210",
+    "url": "/js/app.31cb77d8.js"
+  },
+  {
+    "revision": "17a107c9f1b6371bbe85",
+    "url": "/js/chunk-vendors.ba54a772.js"
+  },
+  {
+    "revision": "764bcd80dde60743abee0867b285d9ee",
+    "url": "/locales/cn-CN/translation.json"
+  },
+  {
+    "revision": "949e504443b3688be4361b1f6fd1a1e6",
+    "url": "/locales/de-DE/translation.json"
+  },
+  {
+    "revision": "ce69d5e99121879ce650067febf65a0b",
+    "url": "/locales/en-EN/translation.json"
+  },
+  {
+    "revision": "de9b524c80f6c97d426ae6a50bc1fbf2",
+    "url": "/locales/es-ES/translation.json"
+  },
+  {
+    "revision": "6d38dd7957c79045d1c2518aa73fb74d",
+    "url": "/locales/fr-FR/translation.json"
+  },
+  {
+    "revision": "294ce3ccea90caa2a9c5f94b8b72c8d5",
+    "url": "/locales/ru-RU/translation.json"
+  },
+  {
+    "revision": "08489a9cfef0d6b8c3ede97cd7be061e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "1547d58e14285b408bec08c48346364b",
+    "url": "/pdf/RusCoin White Paper_040921_eng-converted.pdf"
+  },
+  {
+    "revision": "c2d47526d9f11c832b111bc579d145de",
+    "url": "/pdf/RusCoin White Paper_040921_ru-converted.pdf"
+  },
+  {
+    "revision": "2103232376fe3e1d4a727d97b71a8970",
+    "url": "/pdf/RusCoin White Paper_141221_eng.pdf"
+  },
+  {
+    "revision": "766409f58ab619075632fa46810a9207",
+    "url": "/pdf/RusCoin White Paper_141221_ru.pdf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "74e6ee9a8d6daadb464d8d9124c45e2c",
+    "url": "/ton.json"
+  }
+]);
